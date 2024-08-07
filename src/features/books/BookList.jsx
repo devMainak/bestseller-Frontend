338f41b69796a5735af5ce240c6ce0d4ffead89c
rootKid <137388933+devMainak@users.noreply.github.com>
2024-08-07 05:33:41 +0000

    style: fix card img height

--- src/features/books/BookList.jsx
+++ src/features/books/BookList.jsx
@@ -5,23 +5,23 @@ const BookList = ({ books }) => {
   const { status, error } = useSelector(state => state.books)
   
   return (
-      <div className="py-4 container">
-        {status === "loading" && <div class="spinner-border text-danger" role="status">
-          <span class="visually-hidden">Loading...</span>
+      <div className="py-4 container" style={{width: "75vw"}}>
+        {status === "loading" && <div className="spinner-border text-danger" role="status">
+          <span classname="visually-hidden">Loading...</span>
         </div>}
         {error && <p className="fs-4 fw-normal">{error}</p>}
         <div className="row">
           {books.length > 0 && books.map((book) => {
             return (
               <div className="col-sm-6" key={book._id}>
-                <div className="card mb-3" style={{ maxWidth: "540px" }}>
+                <div className="card mb-3 mx-4" style={{ minxWidth: "640px", minHeight: "310px" }}>
                   <div className="row g-0">
                     <div className="col-md-4">
                       <img
                         src={book.coverImageUrl}
                         class="img-fluid rounded-start"
                         alt={book.title}
-                        style={{ maxHeight: "265px" }}
+                        style={{ minHeight: "310px" }}
                       />
                     </div>
                     <div className="col-md-8">
--- src/features/categories/CategoryList.jsx
+++ src/features/categories/CategoryList.jsx
@@ -10,15 +10,15 @@ const CategoryList = ({categories}) => {
       <section className='bg-danger-subtle'>
         <div className='container'>
         <h3 className='display-3 fw-semibold'>Categories</h3>
-          {status === "loading" && <div class="spinner-border text-danger" role="status">
-            <span class="visually-hidden">Loading...</span>
+          {status === "loading" && <div className="spinner-border text-danger" role="status">
+            <span className="visually-hidden">Loading...</span>
           </div>}
           {error && <p className='fs-4 fw-normal'>{error}</p>}
           { categories.length > 0 && 
             <div className='row pt-2'>
               {categories.map(category => {
                 return (
-                  <div className='col-sm-4 py-2'>
+                  <div className='col-sm-4 py-2' key={category._id}>
                     <Link to={`/books/${category.category}`}>
                       <div className='card d-flex justify-content-center'>
                        <img className='card-img img-fluid' src={category.coverImage} alt={`${category.category} Books`} style={{height: "15rem"}}/>
