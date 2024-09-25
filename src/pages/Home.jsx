91ba070f0b21a768685bf083af1ebbe2c7345a29
bitMan <mrterm404@gmail.com>
2024-09-25 15:57:45 +0530

    feat: path changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
        <section className="pt-4 pb-4">
          <div className='container'>
          <div className="card d-flex justify-content-center">
-           <img className="card-img img-fluid" src="/public/books-hero.jpg" alt="Books"/>
+           <img className="card-img img-fluid" src="/books-hero.jpg" alt="Books"/>
            <div className="card-img-overlay text-light align-self-center">
             <h1 className='text-center display-1 fw-semibold'>Explore Books</h1>
            </div>
