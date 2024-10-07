5704608eb1317ac2fabc3be250d1720922a429db
bitMan <mrterm404@gmail.com>
2024-10-07 16:00:36 +0530

    style: wishlist btns fixed

--- src/features/wishlist/Wishlist.jsx
+++ src/features/wishlist/Wishlist.jsx
@@ -1,90 +1,100 @@
-import { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchWishlist, deleteBookFromWishlistAsync } from './wishlistSlice'
-import Header from "../../components/Header"
-import { Link } from 'react-router-dom'
-import { fetchBooks } from '../books/booksSlice'
-import { fetchCart, addBookToCartAsync, updateBookInCartAsync } from '../cart/cartSlice'
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchWishlist, deleteBookFromWishlistAsync } from "./wishlistSlice";
+import Header from "../../components/Header";
+import { Link } from "react-router-dom";
+import { fetchBooks } from "../books/booksSlice";
+import {
+  fetchCart,
+  addBookToCartAsync,
+  updateBookInCartAsync,
+} from "../cart/cartSlice";
 
 const Wishlist = () => {
   // configuring useDispatch for usage
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   // alert for iteractivity
-  const [alert, setAlert] = useState('')
-  
+  const [alert, setAlert] = useState("");
+
   // Fetching wishlist on page load
   useEffect(() => {
-    dispatch(fetchWishlist())
-    dispatch(fetchBooks())
-    dispatch(fetchCart())
-  }, [])
+    dispatch(fetchWishlist());
+    dispatch(fetchBooks());
+    dispatch(fetchCart());
+  }, []);
 
   // Destructuring the wishlistSlice
-  const { wishlist, status, error } = useSelector(state => state.wishlist)
+  const { wishlist, status, error } = useSelector((state) => state.wishlist);
   // Destructuring the books
-  const { books } = useSelector(state => state.books)
+  const { books } = useSelector((state) => state.books);
   // Destructuring the cart for addition and validation
-  const { cart } = useSelector(state => state.cart)
+  const { cart } = useSelector((state) => state.cart);
 
   // Async function to delete book from wishlist on click of btn
   const handleDeleteFromWishlist = async (bookId) => {
     try {
-      const resultAction = await dispatch(deleteBookFromWishlistAsync(bookId))
-      if (deleteBookFromWishlistAsync.fulfilled.match(resultAction))
-      {
-         setAlert("Removed book from Wishlist.")
+      const resultAction = await dispatch(deleteBookFromWishlistAsync(bookId));
+      if (deleteBookFromWishlistAsync.fulfilled.match(resultAction)) {
+        setAlert("Removed book from Wishlist.");
         setTimeout(() => {
-          setAlert("")
-        }, 2000)
+          setAlert("");
+        }, 2000);
       }
-    } catch(error) {
-      console.error(error)
+    } catch (error) {
+      console.error(error);
     }
-  }
- 
+  };
+
   // Async function to move book from wishlist to cart
   const handleMoveToCart = async (bookToSave) => {
-    const bookToUpdate = cart.find(book => book.title === bookToSave.title)
+    const bookToUpdate = cart.find((book) => book.title === bookToSave.title);
     try {
-    if (bookToUpdate) {
-      const updatedQuantity = Number(bookToUpdate.quantity) + 1; // Ensure quantity is a number
-      const updatedBook = { ...bookToUpdate, quantity: updatedQuantity };
+      if (bookToUpdate) {
+        const updatedQuantity = Number(bookToUpdate.quantity) + 1; // Ensure quantity is a number
+        const updatedBook = { ...bookToUpdate, quantity: updatedQuantity };
 
-      const resultAction = await dispatch(updateBookInCartAsync({bookId: updatedBook._id, book: updatedBook}));
+        const resultAction = await dispatch(
+          updateBookInCartAsync({ bookId: updatedBook._id, book: updatedBook })
+        );
 
-      if (updateBookInCartAsync.fulfilled.match(resultAction))
-      {
-        setAlert("Book quantity updated successfully in cart.")
-        setTimeout(() => {
-          setAlert("");
-        }, 2000);
+        if (updateBookInCartAsync.fulfilled.match(resultAction)) {
+          setAlert("Book quantity updated successfully in cart.");
+          setTimeout(() => {
+            setAlert("");
+          }, 2000);
+        }
+      } else {
+        const resultAction = await dispatch(addBookToCartAsync(bookToSave));
+        if (addBookToCartAsync.fulfilled.match(resultAction)) {
+          setAlert("Added to cart");
+          setTimeout(() => {
+            setAlert("");
+          }, 2000);
+        }
       }
-    } else {
-      const resultAction = await dispatch(addBookToCartAsync(bookToSave))
-      if (addBookToCartAsync.fulfilled.match(resultAction))
-      {
-        setAlert("Added to cart")
-        setTimeout(() => {
-          setAlert("");
-        }, 2000);
-      }
-    }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
-  
+  };
+
   return (
     <>
-      <Header/>
+      <Header />
       <main className="container">
-        <h4 className="display-3 fw-semibold text-center py-4">Your Wishlist <span className='text-danger'>({wishlist.length > 0 ? wishlist.length : 0})</span></h4>
+        <h4 className="display-3 fw-semibold text-center py-4">
+          Your Wishlist{" "}
+          <span className="text-danger">
+            ({wishlist.length > 0 ? wishlist.length : 0})
+          </span>
+        </h4>
         <section>
-          {status === "loading" && <div className="spinner-border text-danger" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </div>}
-          {error && <p className='fs-4 text-center-danger'>{error}</p>}
+          {status === "loading" && (
+            <div className="spinner-border text-danger" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          )}
+          {error && <p className="fs-4 text-center-danger">{error}</p>}
           {alert && (
             <div className="row">
               <div
@@ -106,51 +116,73 @@ const Wishlist = () => {
               </div>
             </div>
           )}
-          {wishlist.length > 0 && 
-            <ul className='list-group pb-5'>
-              {wishlist.map(book => {
-                const matchingBook = books.find(currBook => currBook.title === book.title);  
-                const bookId = matchingBook ? matchingBook._id : null
-              
-              return ( 
-              <li className='list-group-item d-flex justify-content-between' style={{height: "3in"}} key={book._id}>
-                  <div className='d-flex'>
-                    <div>
-                      <Link to={`/books/${book.categoryName}/${bookId}`} state={books}>
-                      <img src={book.coverImageUrl} className='img-fluid' style={{height: "100%"}}/>
-                        </Link>
+          {wishlist.length > 0 && (
+            <ul className="list-group pb-5">
+              {wishlist.map((book) => {
+                const matchingBook = books.find(
+                  (currBook) => currBook.title === book.title
+                );
+                const bookId = matchingBook ? matchingBook._id : null;
+
+                return (
+                  <li
+                    className="list-group-item"
+                    style={{ height: "3in" }}
+                    key={book._id}
+                  >
+                    <div className="d-flex justify-content-between">
+                      <div className="d-flex flex-grow-1">
+                        <div>
+                          <Link
+                            to={`/books/${book.categoryName}/${bookId}`}
+                            state={books}
+                          >
+                            <img
+                              src={book.coverImageUrl}
+                              className="img-fluid"
+                              style={{ height: "250px", width: "150px" }}
+                            />
+                          </Link>
+                        </div>
+                        <div className="mx-4">
+                          <p className="fs-4 fw-normal w-50">{book.title}</p>
+                          <p>by {book.author}</p>
+                          <p className="card-text btn btn-danger">
+                            ⭐ {book.rating.toFixed(1)}
+                          </p>
+                        </div>
                       </div>
-                    <div className='mx-4'>
-                      <p className='fs-4 fw-normal'>{book.title}</p>
-                      <p>by {book.author}</p>
-                      <p className="card-text btn btn-danger">
-                        ⭐ {book.rating.toFixed(1)}
-                      </p>
+                      <div className="flex-grow-2">
+                      <div className="d-grid gap-2">
+                        <button
+                          className="btn btn-danger"
+                          style={{width: "300px"}}
+                          type="button"
+                          onClick={() => handleMoveToCart(book)}
+                        >
+                          Move to Cart
+                        </button>
+                        <button
+                          className="btn btn-danger text-danger bg-danger-subtle"
+                          style={{width: "300px"}}
+                          type="button"
+                          onClick={() => handleDeleteFromWishlist(book._id)}
+                        >
+                          Remove from Wishlist
+                        </button>
+                      </div>
                     </div>
-                  </div>   
-                  <div>
-                    <div className="d-grid gap-2">
-                      <button className="btn btn-danger" type="button" onClick={() => handleMoveToCart(book)}>
-                        Move to Cart
-                      </button>
-                      <button
-                        className="btn btn-light text-danger bg-danger-subtle"
-                        type="button" onClick={() => handleDeleteFromWishlist(book._id)}
-                      >
-                        Remove from Wishlist
-                      </button>
                     </div>
-                  </div>
-      
-              </li>
-                )
-          })}
+                   
+                  </li>
+                );
+              })}
             </ul>
-          }
+          )}
         </section>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Wishlist
+export default Wishlist;
