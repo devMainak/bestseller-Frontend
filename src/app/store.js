c8ddfe1c642fc241e2d730a165ae89c5288611e2
bitMan <mrterm404@gmail.com>
2024-11-04 17:14:10 +0530

    Feat: Purge method to clear the persist status

--- src/app/store.js
+++ src/app/store.js
@@ -1,18 +1,18 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import storage from 'redux-persist/lib/storage' //defaults to localStorage
-import { persistStore, persistReducer } from 'redux-persist'
-import { booksSlice } from "../features/books/booksSlice"
-import { categoriesSlice } from "../features/categories/categoriesSlice"
-import { wishlistSlice } from "../features/wishlist/wishlistSlice"
-import { cartSlice } from "../features/cart/cartSlice"
-import { addressSlice } from "../features/address/adressSlice"
+import storage from "redux-persist/lib/storage"; //defaults to localStorage
+import { persistStore, persistReducer } from "redux-persist";
+import { booksSlice } from "../features/books/booksSlice";
+import { categoriesSlice } from "../features/categories/categoriesSlice";
+import { wishlistSlice } from "../features/wishlist/wishlistSlice";
+import { cartSlice } from "../features/cart/cartSlice";
+import { addressSlice } from "../features/address/adressSlice";
 
 // Defining persist config
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-  whitelist: ['books', 'wishlist', 'cart']
-}
+  whitelist: ["books", "wishlist", "cart"],
+};
 
 // Combining reducers
 const rootReducer = combineReducers({
@@ -20,18 +20,21 @@ const rootReducer = combineReducers({
   categories: categoriesSlice.reducer,
   wishlist: wishlistSlice.reducer,
   cart: cartSlice.reducer,
-  address: addressSlice.reducer
-})
+  address: addressSlice.reducer,
+});
 
 // Creating persisted reducer
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // Configuring the store with the persisted reducer
 const store = configureStore({
-  reducer: persistedReducer
-})
+  reducer: persistedReducer,
+});
 
 // Creating persistor
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
 
-export default store
+// Clear all persisted data
+// persistor.purge();
+
+export default store;
