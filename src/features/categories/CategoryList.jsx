f33d96f0cb264f7562655b639f66c73aedc128b6
bitMan <mrterm404@gmail.com>
2024-11-05 23:24:55 +0530

    style: redesign of the home page

--- src/features/categories/CategoryList.jsx
+++ src/features/categories/CategoryList.jsx
@@ -1,40 +1,57 @@
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-const CategoryList = ({categories}) => {
-  // Destructuring categories slice from global state 
-  const { status, error } = useSelector(state => state.categories)
-  
+const CategoryList = ({ categories }) => {
+  // Destructuring categories slice from global state
+  const { status, error } = useSelector((state) => state.categories);
+
   return (
     <div>
-      <section className='bg-danger-subtle'>
-        <div className='container'>
-        <h3 className='display-3 fw-semibold'>Categories</h3>
-          {status === "loading" && <div className="spinner-border text-danger" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </div>}
-          {error && <p className='fs-4 fw-normal'>{error}</p>}
-          { categories.length > 0 && 
-            <div className='row pt-2'>
-              {categories.map(category => {
+      <section>
+        <div className="container py-5">
+          <h4
+            className="display-4 text-center py-2"
+            style={{ fontFamily: "Noto Serif" }}
+          >
+            <em>Categories</em>
+          </h4>
+          {status === "loading" && (
+            <div className="spinner-border text-danger" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          )}
+          {error && <p className="fs-4 fw-normal">{error}</p>}
+          {categories.length > 0 && (
+            <div className="d-flex justify-content-center gap-2 flex-wrap pt-2 pb-4">
+              {categories.map((category) => {
                 return (
-                  <div className='col-sm-4 py-2' key={category._id}>
+                  <div
+                    className="mt-4"
+                    key={category._id}
+                    style={{ width: "250px", height: "150px" }}
+                  >
                     <Link to={`/books/${category.category}`}>
-                      <div className='card d-flex justify-content-center'>
-                       <img className='card-img img-fluid' src={category.coverImage} alt={`${category.category} Books`} style={{height: "15rem"}}/>
+                      <div>
+                        <img
+                          className="card-img img-fluid"
+                          src={category.coverImage}
+                          alt={`${category.category} Books`}
+                          style={{ height: "150px", borderRadius: "5px" }}
+                        />
                       </div>
                     </Link>
-                    <p className='fs-4 fw-semibold'>{category.category}</p>
+                    <p className="text-center text-dark fw-semibold fs-5">
+                      {category.category}
+                    </p>
                   </div>
-                )
-              })
-            }
+                );
+              })}
             </div>
-          }
-          </div>
+          )}
+        </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryList
+export default CategoryList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,129 @@
-import { useDispatch, useSelector } from 'react-redux'
-import CategoryList from '../features/categories/CategoryList'
-import { useEffect } from 'react'
-import { fetchCategories } from '../features/categories/categoriesSlice'
+import { useDispatch, useSelector } from "react-redux";
+import CategoryList from "../features/categories/CategoryList";
+import { useEffect } from "react";
+import { fetchBooks } from "../features/books/booksSlice";
+import { fetchCategories } from "../features/categories/categoriesSlice";
+import TopRated from "../features/toprated/TopRated";
+import Trending from "../features/trending/Trending";
 
 const Home = () => {
   // configuring useDispatch for usage
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  // Fetching categories for population
+  // Fetching books and categories for population
   useEffect(() => {
-    dispatch(fetchCategories())
-  }, [])
+    dispatch(fetchCategories());
+    dispatch(fetchBooks());
+  }, []);
 
-  const { categories } = useSelector(state => state.categories)
-  
+  const { categories } = useSelector((state) => state.categories);
+
+  /**
+  *  <div className="carousel-item active">
+            <img
+              src="https://faithhogan.com/wp-content/uploads/2016/03/Books-banner.png"
+              className="img-fluid d-block w-100"
+              style={{ height: "500px" }}
+              alt="..."
+            />
+            <div className="card-img-overlay text-light align-self-center">
+              <h1 className="text-center text-danger display-1 fw-semibold">
+                Explore Books
+              </h1>
+            </div>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item">
+              <img
+                src="https://static.vecteezy.com/system/resources/previews/021/916/224/non_2x/promo-banner-with-stack-of-books-globe-inkwell-quill-plant-lantern-ebook-world-book-day-bookstore-bookshop-library-book-lover-bibliophile-education-for-poster-cover-advertising-vector.jpg"
+                className="img-fluid d-block w-100"
+                style={{ height: "500px" }}
+                alt="..."
+              />
+            </div>
+            <div class="carousel-item">
+              <img
+                src="https://i.pinimg.com/564x/e0/90/11/e09011b008b169b474f01bee05328d01.jpg"
+                className="img-fluid d-block w-100"
+                style={{ height: "500px" }}
+                alt="..."
+              />
+            </div>
+  */
+
   return (
     <div>
-       <section className="pt-4 pb-4">
-         <div className='container'>
-         <div className="card d-flex justify-content-center">
-           <img className="card-img img-fluid" src="/books-hero.jpg" alt="Books"/>
-           <div className="card-img-overlay text-light align-self-center">
-            <h1 className='text-center display-1 fw-semibold'>Explore Books</h1>
-           </div>
-         </div>
-           </div>
-           </section>
-          <CategoryList categories={categories} />
+      <section className="">
+        <div
+          id="carouselExampleAutoplaying"
+          className="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img
+                src="../../public/books-hero.jpg"
+                className="img-fluid d-block w-100"
+                style={{ height: "500px" }}
+                alt="..."
+              />
+              <div className="card-img-overlay text-light align-self-center">
+                <h1 className="text-center display-1 fw-semibold">
+                  Explore Books
+                </h1>
+              </div>
+            </div>
+            <div className="carousel-inner">
+              <div className="carousel-item">
+                <img
+                  src="https://static.vecteezy.com/system/resources/previews/021/916/224/non_2x/promo-banner-with-stack-of-books-globe-inkwell-quill-plant-lantern-ebook-world-book-day-bookstore-bookshop-library-book-lover-bibliophile-education-for-poster-cover-advertising-vector.jpg"
+                  className="img-fluid d-block w-100"
+                  style={{ height: "500px" }}
+                  alt="..."
+                />
+              </div>
+              <div class="carousel-item">
+                <img
+                  src="https://i.pinimg.com/564x/e0/90/11/e09011b008b169b474f01bee05328d01.jpg"
+                  className="img-fluid d-block w-100"
+                  style={{ height: "500px" }}
+                  alt="..."
+                />
+              </div>
+            </div>
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleAutoplaying"
+              data-bs-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleAutoplaying"
+              data-bs-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+      </section>
+      <CategoryList categories={categories} />
+      <hr className="container"/>
+      <Trending />
+      <TopRated />
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
